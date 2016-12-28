7ac6ac3cc392a7e86ba2d6bfcfe8ad28d5711e4f
srinikhil <srinikhil.r@gmail.com>
2016-12-28 16:36:53 +0530

    Support for  FieldMaskTree to support repeated Fields in FieldMask, using the notation “Field_name:Offset”

--- java/util/src/main/java/com/google/protobuf/util/FieldMaskTree.java
+++ java/util/src/main/java/com/google/protobuf/util/FieldMaskTree.java
@@ -61,227 +61,296 @@ import java.util.logging.Logger;
  * FieldMask in a message tree.
  */
 final class FieldMaskTree {
-  private static final Logger logger = Logger.getLogger(FieldMaskTree.class.getName());
+	private static final Logger logger = Logger.getLogger(FieldMaskTree.class.getName());
 
-  private static final String FIELD_PATH_SEPARATOR_REGEX = "\\.";
+	private static final String FIELD_PATH_SEPARATOR_REGEX = "\\.";
 
-  private static final class Node {
-    final SortedMap<String, Node> children = new TreeMap<String, Node>();
-  }
+	private static final class Node {
+		final SortedMap<String, Node> children = new TreeMap<String, Node>();
+	}
 
-  private final Node root = new Node();
+	private final Node root = new Node();
 
-  /**
-   * Creates an empty FieldMaskTree.
-   */
-  FieldMaskTree() {}
+	/**
+	 * Creates an empty FieldMaskTree.
+	 */
+	FieldMaskTree() {}
 
-  /**
-   * Creates a FieldMaskTree for a given FieldMask.
-   */
-  FieldMaskTree(FieldMask mask) {
-    mergeFromFieldMask(mask);
-  }
+	/**
+	 * Creates a FieldMaskTree for a given FieldMask.
+	 */
+	FieldMaskTree(FieldMask mask) {
+		mergeFromFieldMask(mask);
+	}
 
-  @Override
-  public String toString() {
-    return FieldMaskUtil.toString(toFieldMask());
-  }
+	@Override
+	public String toString() {
+		return FieldMaskUtil.toString(toFieldMask());
+	}
 
-  /**
-   * Adds a field path to the tree. In a FieldMask, every field path matches the
-   * specified field as well as all its sub-fields. For example, a field path
-   * "foo.bar" matches field "foo.bar" and also "foo.bar.baz", etc. When adding
-   * a field path to the tree, redundant sub-paths will be removed. That is,
-   * after adding "foo.bar" to the tree, "foo.bar.baz" will be removed if it
-   * exists, which will turn the tree node for "foo.bar" to a leaf node.
-   * Likewise, if the field path to add is a sub-path of an existing leaf node,
-   * nothing will be changed in the tree.
-   */
-  FieldMaskTree addFieldPath(String path) {
-    String[] parts = path.split(FIELD_PATH_SEPARATOR_REGEX);
-    if (parts.length == 0) {
-      return this;
-    }
-    Node node = root;
-    boolean createNewBranch = false;
-    // Find the matching node in the tree.
-    for (String part : parts) {
-      // Check whether the path matches an existing leaf node.
-      if (!createNewBranch && node != root && node.children.isEmpty()) {
-        // The path to add is a sub-path of an existing leaf node.
-        return this;
-      }
-      if (node.children.containsKey(part)) {
-        node = node.children.get(part);
-      } else {
-        createNewBranch = true;
-        Node tmp = new Node();
-        node.children.put(part, tmp);
-        node = tmp;
-      }
-    }
-    // Turn the matching node into a leaf node (i.e., remove sub-paths).
-    node.children.clear();
-    return this;
-  }
+	/**
+	 * Adds a field path to the tree. In a FieldMask, every field path matches the
+	 * specified field as well as all its sub-fields. For example, a field path
+	 * "foo.bar" matches field "foo.bar" and also "foo.bar.baz", etc. When adding
+	 * a field path to the tree, redundant sub-paths will be removed. That is,
+	 * after adding "foo.bar" to the tree, "foo.bar.baz" will be removed if it
+	 * exists, which will turn the tree node for "foo.bar" to a leaf node.
+	 * Likewise, if the field path to add is a sub-path of an existing leaf node,
+	 * nothing will be changed in the tree.
+	 */
+	FieldMaskTree addFieldPath(String path) {
+		String[] parts = path.split(FIELD_PATH_SEPARATOR_REGEX);
+		if (parts.length == 0) {
+			return this;
+		}
+		Node node = root;
+		boolean createNewBranch = false;
+		// Find the matching node in the tree.
+		for (String part : parts) {
+			// Check whether the path matches an existing leaf node.
+			if (!createNewBranch && node != root && node.children.isEmpty()) {
+				// The path to add is a sub-path of an existing leaf node.
+				return this;
+			}
+			if (node.children.containsKey(part)) {
+				node = node.children.get(part);
+			} else {
+				createNewBranch = true;
+				Node tmp = new Node();
+				node.children.put(part, tmp);
+				node = tmp;
+			}
+		}
+		// Turn the matching node into a leaf node (i.e., remove sub-paths).
+		node.children.clear();
+		return this;
+	}
 
-  /**
-   * Merges all field paths in a FieldMask into this tree.
-   */
-  FieldMaskTree mergeFromFieldMask(FieldMask mask) {
-    for (String path : mask.getPathsList()) {
-      addFieldPath(path);
-    }
-    return this;
-  }
+	/**
+	 * Merges all field paths in a FieldMask into this tree.
+	 */
+	FieldMaskTree mergeFromFieldMask(FieldMask mask) {
+		for (String path : mask.getPathsList()) {
+			addFieldPath(path);
+		}
+		return this;
+	}
 
-  /**
-   * Converts this tree to a FieldMask.
-   */
-  FieldMask toFieldMask() {
-    if (root.children.isEmpty()) {
-      return FieldMask.getDefaultInstance();
-    }
-    List<String> paths = new ArrayList<String>();
-    getFieldPaths(root, "", paths);
-    return FieldMask.newBuilder().addAllPaths(paths).build();
-  }
+	/**
+	 * Converts this tree to a FieldMask.
+	 */
+	FieldMask toFieldMask() {
+		if (root.children.isEmpty()) {
+			return FieldMask.getDefaultInstance();
+		}
+		List<String> paths = new ArrayList<String>();
+		getFieldPaths(root, "", paths);
+		return FieldMask.newBuilder().addAllPaths(paths).build();
+	}
 
-  /**
-   * Gathers all field paths in a sub-tree.
-   */
-  private void getFieldPaths(Node node, String path, List<String> paths) {
-    if (node.children.isEmpty()) {
-      paths.add(path);
-      return;
-    }
-    for (Entry<String, Node> entry : node.children.entrySet()) {
-      String childPath = path.isEmpty() ? entry.getKey() : path + "." + entry.getKey();
-      getFieldPaths(entry.getValue(), childPath, paths);
-    }
-  }
+	/**
+	 * Gathers all field paths in a sub-tree.
+	 */
+	private void getFieldPaths(Node node, String path, List<String> paths) {
+		if (node.children.isEmpty()) {
+			paths.add(path);
+			return;
+		}
+		for (Entry<String, Node> entry : node.children.entrySet()) {
+			String childPath = path.isEmpty() ? entry.getKey() : path + "." + entry.getKey();
+			getFieldPaths(entry.getValue(), childPath, paths);
+		}
+	}
 
-  /**
-   * Adds the intersection of this tree with the given {@code path} to {@code output}.
-   */
-  void intersectFieldPath(String path, FieldMaskTree output) {
-    if (root.children.isEmpty()) {
-      return;
-    }
-    String[] parts = path.split(FIELD_PATH_SEPARATOR_REGEX);
-    if (parts.length == 0) {
-      return;
-    }
-    Node node = root;
-    for (String part : parts) {
-      if (node != root && node.children.isEmpty()) {
-        // The given path is a sub-path of an existing leaf node in the tree.
-        output.addFieldPath(path);
-        return;
-      }
-      if (node.children.containsKey(part)) {
-        node = node.children.get(part);
-      } else {
-        return;
-      }
-    }
-    // We found a matching node for the path. All leaf children of this matching
-    // node is in the intersection.
-    List<String> paths = new ArrayList<String>();
-    getFieldPaths(node, path, paths);
-    for (String value : paths) {
-      output.addFieldPath(value);
-    }
-  }
+	/**
+	 * Adds the intersection of this tree with the given {@code path} to {@code output}.
+	 */
+	void intersectFieldPath(String path, FieldMaskTree output) {
+		if (root.children.isEmpty()) {
+			return;
+		}
+		String[] parts = path.split(FIELD_PATH_SEPARATOR_REGEX);
+		if (parts.length == 0) {
+			return;
+		}
+		Node node = root;
+		for (String part : parts) {
+			if (node != root && node.children.isEmpty()) {
+				// The given path is a sub-path of an existing leaf node in the tree.
+				output.addFieldPath(path);
+				return;
+			}
+			if (node.children.containsKey(part)) {
+				node = node.children.get(part);
+			} else {
+				return;
+			}
+		}
+		// We found a matching node for the path. All leaf children of this matching
+		// node is in the intersection.
+		List<String> paths = new ArrayList<String>();
+		getFieldPaths(node, path, paths);
+		for (String value : paths) {
+			output.addFieldPath(value);
+		}
+	}
 
-  /**
-   * Merges all fields specified by this FieldMaskTree from {@code source} to {@code destination}.
-   */
-  void merge(Message source, Message.Builder destination, FieldMaskUtil.MergeOptions options) {
-    if (source.getDescriptorForType() != destination.getDescriptorForType()) {
-      throw new IllegalArgumentException("Cannot merge messages of different types.");
-    }
-    if (root.children.isEmpty()) {
-      return;
-    }
-    merge(root, "", source, destination, options);
-  }
+	/**
+	 * Merges all fields specified by this FieldMaskTree from {@code source} to {@code destination}.
+	 */
+	void merge(Message source, Message.Builder destination, FieldMaskUtil.MergeOptions options) {
+		if (source.getDescriptorForType() != destination.getDescriptorForType()) {
+			throw new IllegalArgumentException("Cannot merge messages of different types.");
+		}
+		if (root.children.isEmpty()) {
+			return;
+		}
+		merge(root, "", source, destination, options);
+	}
 
-  /**
-   * Merges all fields specified by a sub-tree from {@code source} to {@code destination}.
-   */
-  private void merge(
-      Node node,
-      String path,
-      Message source,
-      Message.Builder destination,
-      FieldMaskUtil.MergeOptions options) {
-    if (source.getDescriptorForType() != destination.getDescriptorForType()) {
-      throw new IllegalArgumentException(
-          String.format(
-              "source (%s) and destination (%s) descriptor must be equal",
-              source.getDescriptorForType(), destination.getDescriptorForType()));
-    }
+	/**
+	 * Merges all fields specified by a sub-tree from {@code source} to {@code destination}.
+	 */
+	private void merge(
+			Node node,
+			String path,
+			Message source,
+			Message.Builder destination,
+			FieldMaskUtil.MergeOptions options) {
+		if (source.getDescriptorForType() != destination.getDescriptorForType()) {
+			throw new IllegalArgumentException(
+					String.format(
+							"source (%s) and destination (%s) descriptor must be equal",
+							source.getDescriptorForType(), destination.getDescriptorForType()));
+		}
 
-    Descriptor descriptor = source.getDescriptorForType();
-    for (Entry<String, Node> entry : node.children.entrySet()) {
-      FieldDescriptor field = descriptor.findFieldByName(entry.getKey());
-      if (field == null) {
-        logger.warning(
-            "Cannot find field \""
-                + entry.getKey()
-                + "\" in message type "
-                + descriptor.getFullName());
-        continue;
-      }
-      if (!entry.getValue().children.isEmpty()) {
-        if (field.isRepeated() || field.getJavaType() != FieldDescriptor.JavaType.MESSAGE) {
-          logger.warning(
-              "Field \""
-                  + field.getFullName()
-                  + "\" is not a "
-                  + "singluar message field and cannot have sub-fields.");
-          continue;
-        }
-        String childPath = path.isEmpty() ? entry.getKey() : path + "." + entry.getKey();
-        merge(
-            entry.getValue(),
-            childPath,
-            (Message) source.getField(field),
-            destination.getFieldBuilder(field),
-            options);
-        continue;
-      }
-      if (field.isRepeated()) {
-        if (options.replaceRepeatedFields()) {
-          destination.setField(field, source.getField(field));
-        } else {
-          for (Object element : (List) source.getField(field)) {
-            destination.addRepeatedField(field, element);
-          }
-        }
-      } else {
-        if (field.getJavaType() == FieldDescriptor.JavaType.MESSAGE) {
-          if (options.replaceMessageFields()) {
-            if (!source.hasField(field)) {
-              destination.clearField(field);
-            } else {
-              destination.setField(field, source.getField(field));
-            }
-          } else {
-            if (source.hasField(field)) {
-              destination.getFieldBuilder(field).mergeFrom((Message) source.getField(field));
-            }
-          }
-        } else {
-          if (source.hasField(field) || !options.replacePrimitiveFields()) {
-            destination.setField(field, source.getField(field));
-          } else {
-            destination.clearField(field);
-          }
-        }
-      }
-    }
-  }
+		Descriptor descriptor = source.getDescriptorForType();
+		String key ;
+		int offset ; 
+		Message arr_src_msg = null;
+		for (Entry<String, Node> entry : node.children.entrySet()) {
+
+			key = entry.getKey();
+			/**Changes for ZDocs 
+			 * ----- 
+			 * Support for merging data in repeated fields
+			 * ----- 
+			 * Start *
+			 * */
+			offset = -1;
+			if(key.contains(":")){
+				key = key.split(":")[0];
+				offset = Integer.parseInt(key.split(":")[1]);
+			}
+			/**Changes for ZDocs 
+			 * --
+			 * End *
+			 * */
+			FieldDescriptor field = descriptor.findFieldByName(key);
+			if (field == null) {
+				logger.warning(
+						"Cannot find field \""
+								+ entry.getKey()
+								+ "\" in message type "
+								+ descriptor.getFullName());
+				continue;
+			}
+			if (!entry.getValue().children.isEmpty()) {
+				if (field.getJavaType() != FieldDescriptor.JavaType.MESSAGE) {
+					logger.warning(
+							"Field \""
+									+ field.getFullName()
+									+ "\" is not a "
+									+ "singluar message field and cannot have sub-fields.");
+					continue;
+				}
+				String childPath = path.isEmpty() ? entry.getKey() : path + "." + entry.getKey();
+				/**Changes for ZDocs 
+				 * ----- 
+				 * Support for merging data in repeated fields
+				 * ----- 
+				 * Start *
+				 */
+				if(field.isRepeated()){
+					arr_src_msg = (Message) ((List)source.getField(field)).get(0);
+					merge(
+							entry.getValue(),
+							childPath,
+							arr_src_msg,
+							destination.getRepeatedFieldBuilder(field, offset),
+							options);
+					continue;
+				}
+				/**Changes for ZDocs 
+				 * --
+				 * End *
+				 * */
+				merge(
+						entry.getValue(),
+						childPath,
+						(Message) source.getField(field),
+						destination.getFieldBuilder(field),
+						options);
+				continue;
+			}
+			if (field.isRepeated()) {
+				/**Changes for ZDocs 
+				 * ----- 
+				 * Support for merging data in repeated fields
+				 * ----- 
+				 * Start *
+				 */
+				if(offset!= -1){
+					try{
+						arr_src_msg = (Message) ((List)source.getField(field)).get(0);
+					}catch(ArrayIndexOutOfBoundsException e){
+						/** Use case:
+						 * Deleting an object from the repeated field.
+						 */
+						destination.getRepeatedFieldBuilder(field, offset).clear();
+						continue;
+					}
+					/** Use Case:
+					 * Adding an Object to a repeated field.
+					 */
+					if(offset >=  destination.getRepeatedFieldCount(field)){
+						destination.addRepeatedField(field, arr_src_msg);
+						continue;
+					}
+				}
+				/**Changes for ZDocs 
+				 * --
+				 * End *
+				 * */
+				if (options.replaceRepeatedFields()) {
+					destination.setField(field, source.getField(field));
+				} else {
+					for (Object element : (List) source.getField(field)) {
+						destination.addRepeatedField(field, element);
+					}
+				}
+			} else {
+				if (field.getJavaType() == FieldDescriptor.JavaType.MESSAGE) {
+					if (options.replaceMessageFields()) {
+						if (!source.hasField(field)) {
+							destination.clearField(field);
+						} else {
+							destination.setField(field, source.getField(field));
+						}
+					} else {
+						if (source.hasField(field)) {
+							destination.getFieldBuilder(field).mergeFrom((Message) source.getField(field));
+						}
+					}
+				} else {
+					if (source.hasField(field) || !options.replacePrimitiveFields()) {
+						destination.setField(field, source.getField(field));
+					} else {
+						destination.clearField(field);
+					}
+				}
+			}
+		}
+	}
 }
+
